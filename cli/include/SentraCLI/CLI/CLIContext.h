2a77ae86575a3bb0eeb7b0d0d13c7532e5245964
ABDELBAKI Boukerche <boukerche.abdelbaki@gmail.com>
2026-07-18 09:44:52 +0100

    Implemented CLIContext

---
+++ cli/include/SentraCLI/CLI/CLIContext.h
@@ -0,0 +1,34 @@
+//
+// Created by Abdelbaki Boukerche on 18/7/2026.
+//
+
+#ifndef SENTRA_ENGINE_CLICONTEXT_H
+#define SENTRA_ENGINE_CLICONTEXT_H
+
+#include <string>
+
+namespace SCLI {
+    enum class CLIMode {
+        Exec,
+        Config,
+        InterfaceConfig,
+    };
+
+    class CLIContext {
+    public:
+        inline CLIMode GetCLIMode() const { return m_Mode; }
+        void SetCLIMode(CLIMode mode);
+
+        inline bool QuitRequested() const { return m_QuitRequested; }
+        void RequestQuit();
+
+        std::string GetPrompt() const;
+
+    private:
+        CLIMode m_Mode = CLIMode::Exec;
+
+        bool m_QuitRequested = false;
+    };
+} // SCLI
+
+#endif //SENTRA_ENGINE_CLICONTEXT_H
---
+++ cli/src/CLI/CLIContext.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Abdelbaki Boukerche on 18/7/2026.
+//
+
+#include "SentraCLI/CLI/CLIContext.h"
+
+namespace SCLI {
+    void CLIContext::SetCLIMode(const CLIMode mode) {
+        m_Mode = mode;
+    }
+
+    void CLIContext::RequestQuit() {
+        m_QuitRequested = true;
+    }
+
+    std::string CLIContext::GetPrompt() const {
+        switch (m_Mode) {
+            case CLIMode::Exec:
+                return "Sentra# ";
+            case CLIMode::Config:
+                return "Sentra(config)# ";
+            case CLIMode::InterfaceConfig:
+                return "Sentra(config-if)# ";
+        }
+        return "Sentra# ";
+    }
+} // SCLI
